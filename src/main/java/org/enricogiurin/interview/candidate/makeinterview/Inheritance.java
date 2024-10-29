e693dc1dc9acc5fc4564461dfeb288d43a0c2ed2
Enrico Giurin <enricogiurin@gmail.com>
2024-10-30 00:21:03 +0100

    minor fixes

---
+++ src/main/java/org/enricogiurin/interview/candidate/makeinterview/Inheritance.java
@@ -0,0 +1,19 @@
+package org.enricogiurin.interview.candidate.makeinterview;
+
+public class Inheritance {
+//Q: Are constructors inherited in Java? Why or why not?
+
+}
+
+class A {
+  private int x;
+  A(int x){
+    this.x=x;
+  }
+
+}
+
+/*class B extends A {
+
+
+}*/
--- src/main/java/org/enricogiurin/interview/candidate/makeinterview/UsageOfThreads.java
+++ src/main/java/org/enricogiurin/interview/candidate/makeinterview/UsageOfThreads.java
@@ -4,6 +4,7 @@ public class UsageOfThreads {
     public static void main(String[] args) {
         //start a thread with an instance of MyRunnable
         MyRunnable mr = new MyRunnable();
+        //Thread t
     }
 }
 
